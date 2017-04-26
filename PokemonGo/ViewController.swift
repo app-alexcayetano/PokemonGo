f35b19e82f8cedb73a0cd5fa4bc54586a6e41aa5
Alexander Cayetano <app.alexcayetano@gmail.com>
2017-04-25 23:41:20 -0500

    - Implementando la logica del mapa - Añadiendo los gráficos del videoJuego

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    var updateCount = 0
+    
+    let mapDistance : CLLocationDistance = 300
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Estamos listos para salir a cazar Pokemons")
+            self.mapView.showsUserLocation = true
+            self.manager.stopUpdatingLocation()
+        }else {
+            self.manager.requestWhenInUseAuthorization() //Pide solo cuando la app esta en uso
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: Core Location Manager Delegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if updateCount < 4 {
+            
+            if let coordinate = self.manager.location?.coordinate {
+                let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, mapDistance, mapDistance)
+                self.mapView.setRegion(region, animated: true)
+                 updateCount += 1
+            }
+           
+        } else {
+            self.manager.stopUpdatingLocation()
+        }
+        
+        
+    }
+    
+    @IBAction func updateUserLocation(_ sender: UIButton) {
+        if let coordinate = self.manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, mapDistance, mapDistance)
+            self.mapView.setRegion(region, animated: true)
+        }
+        
+    }
+    
+    
 
 }
 
